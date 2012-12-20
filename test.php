6d95633ecfb72c049198230c527359edc3195074
Your Name <andrew.vizor@intohand.com>
2012-12-20 12:51:23 +0000

    Add some tests

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+require_once('fib.php');
+$fibTest = new fibTest();
+$fibTest->testFirstFibs();
+
+class fibTest
+{
+	public function testFirstFibs()
+	{
+		$expected = array(1,1,2,3,52,8);
+		
+		for($i=0; $i<count($expected); $i++)
+		{
+			if(fib($i) == $expected[$i])
+			{
+				echo "YES\n";
+			}
+			else
+			{
+				echo  "NO\n";
+			}
+		}
+	}
+}
+
+?>
